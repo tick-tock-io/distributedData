3bc3f5e6c6c5ad5aa37eb385c771f8c73de24acd
liushuangbin3 <liushuangbin3@jd.com>
2020-12-17 11:12:58 +0800

    修改无用注释

--- src/main/java/com/jdd/distributedData/distributedData/controller/OrderController.java
+++ src/main/java/com/jdd/distributedData/distributedData/controller/OrderController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
-
 @RestController
 public class OrderController {
 
